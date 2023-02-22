d29d8cb6f39e7360daf885b4b5482a2693eaaacc
waseemakaram71 <waseemsharani71@gmail.com>
2023-02-22 12:31:12 +0500

    assignment 9

---
+++ src/assignment9.js
@@ -0,0 +1,42 @@
+// let num = 2.55;
+//  Math.round(num);
+// document.write(Math.round(num));
+// let number = prompt("enter the number");
+// Math.round(number);
+// document.write(Math.round(number));
+// let number = prompt("enter the number");
+// Math.ceil(number);
+// document.write(Math.ceil(number));
+// let number = prompt("enter the number");
+// Math.floor(number);
+// document.write(Math.floor(number));
+// let number = prompt("enter the negetive number");
+// Math.round(number);
+// document.write(Math.round(number));
+// let number = prompt("enter the number");
+// Math.ceil(number);
+// document.write(Math.ceil(number));
+// let number = prompt("enter the number");
+// Math.floor(number);
+// document.write(Math.floor(number));
+// let number = -5;
+// Math.abs(number);
+// document.write(Math.abs(number));
+
+// const diceValue = (Math.ceil(Math.random () * 6));
+// document .write("random dice value "  + `${diceValue}`);
+
+// document.write(Math.ceil(Math.random() * 2));
+
+// document .write(Math.floor(Math.random() * 100));
+
+// let weight = prompt("enter the user weight");
+// document.write("the weight of the user is "  + `${weight}` +  "  kg");
+
+// let number = +prompt("eneter the number");
+// const secrt = Math.ceil(Math.random() * 10);
+// if (number == secrt) {
+//   alert("congrats");
+// } else {
+//   alert("tryagain");
+// }
