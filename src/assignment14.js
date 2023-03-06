28358974b7e93fae255a2fde1d0a544938ba61bf
waseemakram71 <waseemsharani71@gmail.com>
2023-03-06 21:23:16 +0500

    update assignment14

---
+++ src/assignment14.js
@@ -0,0 +1,65 @@
+//  function myFunction() {
+//   document.getElementById("sign").submit();
+//   alert("congrats")
+// }
+// var a = myFunction();
+// document.write(a);
+
+
+
+
+
+
+
+
+
+
+
+//  <div class="item">
+//   <h2>Item Title</h2>
+//   <p class="summary">Summary of item details...</p>
+//   <p class="details">Full details of item...</p>
+//   <button class="read-more">Read more</button>
+// </div> 
+
+
+// // // i.
+// let mainContent = document.getElementById("main-content");
+
+// // ii.
+// let childElements = mainContent.children;
+// console.log(childElements);
+
+// // iii.
+// let renderElements = document.getElementsByClassName("render");
+// for (let i = 0; i < renderElements.length; i++) {
+//   console.log(renderElements[i].innerHTML);
+// }
+
+// // iv.
+// document.getElementById("first-name").value = "John";
+
+// // v.
+// document.getElementById("last-name").value = "Doe";
+// document.getElementById("email").value = "johndoe@example.com";
+
+
+// let lastName = document.getElementById("lastName");
+// let textNode = lastName.childNodes[0];
+// textNode.textContent = "Last Name: Smith";
+
+// let mainContent = document.getElementById("main-content");
+// let firstChild = mainContent.firstChild;
+// let lastChild = mainContent.lastChild;
+
+// let lastName = document.getElementById("lastName");
+// let nextSibling = lastName.nextSibling;
+// let previousSibling = lastName.previousSibling;
+
+// let email = document.getElementById("email");
+// let parentNode = email.parentNode;
+// let nodeType = email.nodeType;
+
+
+// let value = document.getElementById('val');
+// console.log('value');
